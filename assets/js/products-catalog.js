00dd8c04743cf5bfe14f62cab4cb60901e9fa51d
Giorgos Prodromou <giorgosprodromou1822@gmail.com>
2026-07-09 11:10:59 +0300

    update Πέμ 09/07/2026 11:10:59,08

---
+++ assets/js/products-catalog.js
@@ -0,0 +1,46 @@
+window.PRODUCT_CATALOG = {
+  "gearbox-6m-hatch": {
+    name: "Gearbox 6-Speed Manual - Hatchback",
+    description: "Καθαρισμένο, ελεγμένο και έτοιμο για τοποθέτηση.",
+    specs: [
+      "Τύπος: Manual 6 σχέσεων",
+      "Κατάσταση: Ελεγμένο",
+      "Εγγύηση: Κατόπιν συνεννόησης"
+    ],
+    images: ["g1", "g2", "g3", "g4"],
+    stock: 3
+  },
+  "gearbox-6a-sedan": {
+    name: "Gearbox 6-Speed Automatic - Sedan",
+    description: "Δοκιμασμένο σε πίεση και σε θερμοκρασία λειτουργίας.",
+    specs: [
+      "Τύπος: Automatic 6 σχέσεων",
+      "Κατάσταση: Ελεγμένο",
+      "Εγγύηση: Κατόπιν συνεννόησης"
+    ],
+    images: ["s1", "s2", "s3", "s4"],
+    stock: 2
+  },
+  "gearbox-5m-suv": {
+    name: "Gearbox 5-Speed Manual - SUV",
+    description: "Με έλεγχο γραναζιών και καθαρό ιστορικό συντήρησης.",
+    specs: [
+      "Τύπος: Manual 5 σχέσεων",
+      "Κατάσταση: Ελεγμένο",
+      "Εγγύηση: Κατόπιν συνεννόησης"
+    ],
+    images: ["u1", "u2", "u3", "u4"],
+    stock: 4
+  },
+  "gearbox-7a-premium": {
+    name: "Gearbox 7-Speed Automatic - Premium Line",
+    description: "Πλήρως ανακατασκευασμένο με έλεγχο από την ομάδα μας.",
+    specs: [
+      "Τύπος: Automatic 7 σχέσεων",
+      "Κατάσταση: Ανακατασκευασμένο",
+      "Εγγύηση: Κατόπιν συνεννόησης"
+    ],
+    images: ["p1", "p2", "p3", "p4"],
+    stock: 1
+  }
+};
